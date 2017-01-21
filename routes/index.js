433c9c99a2400dfefc6fe2ace37d36901aa5f9c5
Artem Boyur <gboyur@gmail.com>
2017-01-21 11:51:46 +0300

    start page ready

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('welcome', { title: 'Елизавета Боюр' });
 });
 
 router.get('/gui', function (req, res) {
